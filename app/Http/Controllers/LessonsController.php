88255b8dc22fd2cfc5c847e86cf1d62506b70b31
stilly <742163033@qq.com>
2018-02-19 21:54:02 +0800

    一些错误整理

--- app/Http/Controllers/LessonsController.php
+++ app/Http/Controllers/LessonsController.php
@@ -20,11 +20,8 @@ class LessonsController extends Controller
     public function index()
     {
 
-        /**
-         * 1.要有状态码和提示信息
-         * 2.不要暴露数据库字段
-         */
         $lessons = Lesson::all();
+
         return Response::json([
             'code' => 200,
             'message' => 'success',
@@ -33,22 +30,13 @@ class LessonsController extends Controller
 
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
         //
@@ -58,43 +46,28 @@ class LessonsController extends Controller
     public function show($id)
     {
         $lesson = Lesson::findOrFail($id);
-        //dd($lesson);
+
         return Response::json([
             'code' => 200,
             'message' => 'success',
             'data' => $this->lessonTransformer->transform($lesson)
         ]);
+
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy($id)
     {
         //
