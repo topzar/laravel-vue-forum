4fb7a0b01168ea8ae5923c36eed878624ada8e88
stilly <742163033@qq.com>
2018-02-19 19:26:51 +0800

    lesson 数据填充配置

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,11 +13,19 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+/*$factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+    ];
+});*/
+
+$factory->define(App\Lesson::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph,
+        'free' => $faker->boolean
     ];
 });
