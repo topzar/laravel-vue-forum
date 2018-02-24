03160ff244da83c00ceefe17659ba52225854012
stilly <742163033@qq.com>
2018-02-24 15:57:32 +0800

    用户表数据迁移，填充文件

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,15 +17,10 @@ $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'password' => bcrypt('password'),
+        'confirmation_token' => str_random(20),
+        'avatar' => $faker->imageUrl(120,120),
+        'experience' => $faker->numberBetween(1,999)
     ];
 });
 
-$factory->define(App\Lesson::class, function (Faker $faker) {
-    return [
-        'title' => $faker->sentence,
-        'body' => $faker->paragraph,
-        'free' => $faker->boolean
-    ];
-});
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,21 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name')->unique()->comment('用户名');
+            $table->string('email')->unique()->comment('邮箱');
             $table->string('password');
-            $table->rememberToken();
+            $table->string('avatar')->default('/resources/images/default.jpg')->comment('用户头像');
+            $table->string('confirmation_token')->comment('激活标识');
+            $table->integer('questions_count')->default(0)->comment('问题总数');
+            $table->integer('answers_count')->default(0)->comment('答案总数');
+            $table->integer('comments_count')->default(0)->comment('评论总数');
+            $table->integer('favorites_count')->default(0)->comment('收藏总数');
+            $table->integer('likes_count')->default(0)->comment('点赞总数');
+            $table->integer('followers_count')->default(0)->comment('关注者总数');
+            $table->integer('followings_count')->default(0)->comment('关注总数');
+            $table->smallInteger('is_active')->default(0)->comment('是否激活');
+            $table->integer('experience')->comment('积分');
+            $table->enum('sex', ['male', 'female']);
             $table->timestamps();
         });
     }
