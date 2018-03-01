0d2fd32e95b39c9d5299c947491957ba0dd183d7
stilly <742163033@qq.com>
2018-03-01 22:34:31 +0800

    topic factory images field

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -6,6 +6,7 @@ $factory->define(\App\Topic::class, function (Faker $faker) {
     return [
         'name' => $faker->word,
         'bio' => $faker->paragraph,
+        'image' => $faker->imageUrl(120,120),
         'questions_count' => 1
     ];
 });
