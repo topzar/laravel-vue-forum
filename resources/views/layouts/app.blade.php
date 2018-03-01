0615b9f2cbcbb963e210a9fed46d4f7e56bc89ee
stilly <742163033@qq.com>
2018-03-01 10:11:53 +0800

    create question button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,9 +43,10 @@
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
+                            <li><a href="{{ route('login') }}">登录</a></li>
+                            <li><a href="{{ route('register') }}">注册</a></li>
                         @else
+                            <a class="btn btn-primary" href="{{ route('question.create') }}">发布问题</a>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
                                     {{ Auth::user()->name }} <span class="caret"></span>
