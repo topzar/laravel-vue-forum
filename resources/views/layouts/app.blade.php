25299bf6f5359cf47df1bf3e3e20c8d5369c7960
stilly <742163033@qq.com>
2018-03-02 16:59:24 +0800

    修订一些样式问题

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        LearnFans
                     </a>
                 </div>
 
@@ -46,7 +46,6 @@
                             <li><a href="{{ route('login') }}">登录</a></li>
                             <li><a href="{{ route('register') }}">注册</a></li>
                         @else
-                            <a class="btn btn-primary" href="{{ route('question.create') }}">发布问题</a>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
                                     {{ Auth::user()->name }} <span class="caret"></span>
--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -1,11 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container-fluid questions-bar">
+        <div class="container">
+            <h3>
+                论坛
+                <a href="{{ route('question.create') }}" class="btn btn-success pull-right"> 发布问题</a>
+            </h3>
+        </div>
+    </div>
     <div class="container">
         <div class="row">
             <div class="col-md-8">
                 @foreach($questions as $question)
-                    <div class="media">
+                    <div class="media questions-list">
                         <div class="media-left">
                             <a href="#">
                                 <img class="media-object img-rounded" src="{{ asset($question->user->avatar) }}" alt="..." height="60px">
@@ -24,9 +32,8 @@
                 @endforeach
             </div>
             <div class="col-md-4">
-                <div class="alert label-info">some infos here</div>
+                <a href="{{ route('question.create') }}"> 登录发布问题</a>
             </div>
         </div>
-    </div>
     </div>
 @endsection
